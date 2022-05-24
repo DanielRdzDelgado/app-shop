ad36305af64263cbc4f2a97bbfa0729977dd3006
Daniel1987rdzd <you@example.com>
2022-05-24 15:29:24 -0500

    Base project

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    //definimos un metodo
+    public function welcome(){
+        return view('welcome');
+    }
+}
